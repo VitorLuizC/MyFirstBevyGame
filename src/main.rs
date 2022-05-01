ef60528fb12f53610825472c64adf23159932c8c
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2022-05-01 19:20:09 -0300

    ✨ change window size and enable vsync

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 use bevy::prelude::*;
+use bevy::window::PresentMode;
+
+pub const CLEAR: Color = Color::rgb(0.0, 0.0, 0.0);
 
 fn main() {
-    App::new().add_plugins(DefaultPlugins).run();
+    App::new()
+        .insert_resource(ClearColor(CLEAR))
+        .insert_resource(WindowDescriptor {
+            width: 1600.0,
+            height: 900.0,
+            title: "MyFirstBevyGame".to_string(),
+
+            // 'vsync' was replaced by 'present_mode' in Bevy 0.7.0, and to have
+            // same behavior its value must be 'PresentMode::Fifo'.
+            present_mode: PresentMode::Fifo,
+            resizable: false,
+            ..Default::default()
+        })
+        .add_plugins(DefaultPlugins)
+        .run();
 }
